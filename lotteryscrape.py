e4cc5415c3b78ef73b124d7a6fc4c57c0956273f
DarrenZastruga <darren.zastruga@gmail.com>
2023-06-09 10:10:47 -0400

    Testing flow to see if OH is working or not

--- lotteryscrape.py
+++ lotteryscrape.py
@@ -5843,24 +5843,24 @@ for t in prizetypes:
         clusterloop(ratingstable, scratchertables, t, std, 2)        
 '''
 
-try: 
-    exportVAScratcherRecs()
-    exportAZScratcherRecs()
-    exportMOScratcherRecs()
-    exportOKScratcherRecs()
-    exportCAScratcherRecs()
-    exportNMScratcherRecs()
-    exportMDScratcherRecs()
-    exportNYScratcherRecs()
-    exportDCScratcherRecs()
-    exportNCScratcherRecs()
-    exportFLScratcherRecs()
-    exportILScratcherRecs()
-    exportKSScratcherRecs()
-    exportOHScratcherRecs()
-    exportTXScratcherRecs()
-except:
-    pass
+#try: 
+exportVAScratcherRecs()
+exportAZScratcherRecs()
+exportMOScratcherRecs()
+exportOKScratcherRecs()
+exportCAScratcherRecs()
+exportNMScratcherRecs()
+exportMDScratcherRecs()
+exportNYScratcherRecs()
+exportDCScratcherRecs()
+exportNCScratcherRecs()
+exportFLScratcherRecs()
+exportILScratcherRecs()
+exportKSScratcherRecs()
+exportOHScratcherRecs()
+exportTXScratcherRecs()
+#except:
+#    pass
 
 now = datetime.now(tzlocal()).strftime('%Y-%m-%d %H:%M:%S %Z')
 logger.info(f'Finishing lotteryscrape.py at: {now}')
